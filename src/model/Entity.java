c8734685a883dd64708d7ba36e28ae4b91b70202
Unknown <michalr835@gmail.com>
2017-10-21 23:52:33 +0200

    dodanie i_entity i entity

---
+++ src/model/Entity.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Entity implements IEntity{
+    private int id;
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+}
