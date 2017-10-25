5c094052077ce8b3a771f12a6708e309d1f0785e
Michal Rotko <michalr835@gmail.com>
2017-10-26 00:03:44 +0200

    dostosowanie maina

--- src/BankSupervisor.java
+++ src/BankSupervisor.java
@@ -1,28 +1,33 @@
+import exception.InputLengthException;
+import model.*;
+import operation.Repository;
+import session.Session;
+
 import java.util.Arrays;
 import java.util.Optional;
 
 public class BankSupervisor {
 
-    public static void main(String [] args) {
-        BankClientData personalData01 = new PersonalData();
+    public static void main(String [] args) throws InputLengthException {
+        PersonalData personalData01 = new PersonalData();
         personalData01.setName("Piotr");
  		personalData01.setSurname("Jakis");
- 		personalData01.setPesel("97537156310l");
+ 		personalData01.setPesel("97537156310");
 
-        BankClientData personalData02 = new PersonalData();
+		PersonalData personalData02 = new PersonalData();
  		personalData02.setName("Zbigniew");
  		personalData02.setSurname("Kot");
- 		personalData02.setPesel("97537156310l");
+ 		personalData02.setPesel("97537156310");
 
- 		BankClientData personalData03 = new PersonalData();
+		PersonalData personalData03 = new PersonalData();
  		personalData03.setName("Zbigniew");
  		personalData03.setSurname("Him");
- 		personalData03.setPesel("6392785492l");
+ 		personalData03.setPesel("63927854921");
 
-        BankClientData companyData01 = new CompanyData();
+        CompanyData companyData01 = new CompanyData();
         companyData01.setName("Ta Firma");
         companyData01.setAddress("Jakaœ 20");
-        companyData01.setNIP("37592039746");
+        companyData01.setNIP("7592039746");
 
         BankClient client0 = new PersonalBankClient();
  		BankClient client1 = new CompanyBankClient();
@@ -43,23 +48,13 @@ public class BankSupervisor {
 
         Repository repository = new Repository();
 
-        Account acc0 = new Account(client0);
- 		acc0.setBalance(1000);
-
- 		Account acc00 = new Account(client0);
- 		acc00.setBalance(11000);
-
- 		Account acc1 = new Account(client1);
- 		acc1.setBalance(20000);
-
- 		Account acc2 = new Account(client2);
- 		acc1.setBalance(3000);
-
- 		Account acc3 = new Account(client3);
- 		acc1.setBalance(4000);
-
-        repository.addBankClients(Arrays.asList(client0, client1, client2, client3));
-        repository.addBankAccounts(Arrays.asList(acc0, acc1, acc2, acc3));
+        BankAccount acc0 = new BankAccount(client0);
+ 		BankAccount acc00 = new BankAccount(client0);
+		BankAccount acc1 = new BankAccount(client1);
+		BankAccount acc2 = new BankAccount(client2);
+		BankAccount acc3 = new BankAccount(client3);
+        repository.addClientList(Arrays.asList(client0, client1, client2, client3));
+        repository.addAccountList(Arrays.asList(acc0, acc1, acc2, acc3));
 
         boolean isDataExistsTest = true;
 
@@ -80,16 +75,17 @@ public class BankSupervisor {
         }
 
         Optional.ofNullable(session.getExecutor()).ifPresent(exe -> {
-            System.out.println(exe.getBalanse());
 
-            BankAccount acc = exe.getUserBankAccounts().get(0);
-            BankAccount accOther = exe.getBankAccountById(0);   // numer wcześniej powinien być znany
+            BankAccount acc = session.getUserBankAccounts().get(0);
+            System.out.println(acc.getBalance());
+            BankAccount accOther = session.getRepository().getAccount(0);   // numer wcześniej powinien być znany
 
-            exe.makeWithdraw(accNum.getBankNumber(), 100);
-            System.out.println(exe.getBalance());
+            exe.makeWithdraw(acc.getBankAccountNumber(), 100);
+            System.out.println(acc.getBalance());
 
-            exe.makeTransfer(accNum.getBankNumber(), accOther.getBankNumber(), 255);
-            System.out.print(exe.getBalance());
+            exe.makeTransfer(acc.getBankAccountNumber(), accOther.getBankAccountNumber(), "pierwszy przelew", 255);
+            System.out.println(acc.getBalance());
+            exe.printLog();
         });
     }
 }
